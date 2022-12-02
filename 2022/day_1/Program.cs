7c34526599a5ea800428542039a7edf3bd7a89c3
davidvajda <davidvajda1998@gmail.com>
2022-12-02 09:59:31 +0100

    day-2

---
+++ 2022/day_1/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace day_1;
+class Program
+{
+    static int MaxCalories(string[] lines)
+    {
+        int maxCalories = 0;
+        int currentCalories = 0;
+        foreach (string line in lines)
+        {
+            if (line == "")
+            {
+                maxCalories = Math.Max(maxCalories, currentCalories);
+                currentCalories = 0;
+                continue;
+            }
+
+            currentCalories += Int32.Parse(line);
+        }
+        return Math.Max(maxCalories, currentCalories);
+    }
+
+    static int CountThreeLargest(string[] lines)
+    {
+        int caloriesSum = 0;
+        var caloriesSumList = new List<int> { };
+
+        foreach (string line in lines)
+        {
+            if (line == "")
+            {
+                caloriesSumList.Add(caloriesSum);
+                caloriesSum = 0;
+                continue;
+            }
+
+            caloriesSum += Int32.Parse(line);
+        }
+        caloriesSumList.Add(caloriesSum);
+
+        caloriesSumList.Sort();
+        caloriesSumList.Reverse();
+        return caloriesSumList.Take(3).Sum();
+    }
+
+
+    static void Main(string[] args)
+    {
+        string[] testLines = System.IO.File.ReadAllLines("./test-input.txt");
+        string[] lines = System.IO.File.ReadAllLines("./input.txt");
+
+        // Part 1
+        // Console.WriteLine($"Highest value in test -> {MaxCalories(testLines)}");
+        // Console.WriteLine($"Highest value in input -> {MaxCalories(lines)}");
+
+        // Part 2
+        Console.WriteLine($"Sum of three highest in test -> {CountThreeLargest(testLines)}");
+        Console.WriteLine($"Sum of three highest in input -> {CountThreeLargest(lines)}");
+    }
+}
---
+++ 2022/day_2/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace day_2;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] testInput = System.IO.File.ReadAllLines("./test-input.txt");
+        string[] input = System.IO.File.ReadAllLines("./input.txt");
+
+        // Part 1
+        Console.WriteLine("Part 1 - Test input score: " + getScore1(testInput));
+        Console.WriteLine("Part 1 - Input score: " + getScore1(input));
+
+        // Part 2
+        Console.WriteLine("Part 2 - Test input score: " + getScore2(testInput));
+        Console.WriteLine("Part 2 - Input score: " + getScore2(input));
+    }
+
+    static int getScore1(string[] moves)
+    {
+        var resultScore = new Dictionary<char, Dictionary<char, int>>(){
+            {'X', new Dictionary<char, int>() {{'A', 3}, {'B', 0}, {'C', 6}}},
+            {'Y', new Dictionary<char, int>() {{'A', 6}, {'B', 3}, {'C', 0}}},
+            {'Z', new Dictionary<char, int>() {{'A', 0}, {'B', 6}, {'C', 3}}},
+        };
+        var moveScore = new Dictionary<char, int>() { { 'X', 1 }, { 'Y', 2 }, { 'Z', 3 } };
+
+        int scoreCount = 0;
+        for (int i = 0; i < moves.Length; i++) {
+            char oponentMove = moves[i][0];
+            char myMove = moves[i][2];
+        scoreCount += resultScore[myMove][oponentMove] + moveScore[myMove];
+        }
+
+        return scoreCount;
+    }
+
+    static int getScore2(string[] moves)
+    {
+        var moveScore = new Dictionary<char, int>() { { 'X', 0 }, { 'Y', 3 }, { 'Z', 6 } };
+        var resultScore = new Dictionary<char, Dictionary<char, int>>(){
+            {'X', new Dictionary<char, int>() {{'A', 3}, {'B', 1}, {'C', 2}}},
+            {'Y', new Dictionary<char, int>() {{'A', 1}, {'B', 2}, {'C', 3}}},
+            {'Z', new Dictionary<char, int>() {{'A', 2}, {'B', 3}, {'C', 1}}},
+        };
+
+        int scoreCount = 0;
+        for (int i = 0; i < moves.Length; i++) {
+            char oponentMove = moves[i][0];
+            char widhedResult = moves[i][2];
+        scoreCount += resultScore[widhedResult][oponentMove] + moveScore[widhedResult];
+        }
+        return scoreCount;
+    }
+}
